c7580c7cf6d55f7fa8af67f1e5986d95e932ec61
dullmajesty <sumariamariahshannen@gmail.com>
2024-09-12 20:02:46 +0800

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ToDoList from './screens/ToDoList';
+import Dashboard from './screens/Dashboard';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ToDoList">
+        
+        <Stack.Screen name="ToDoList" component={ToDoList} />
+        <Stack.Screen name="Dashboard" component={Dashboard}
+           
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
